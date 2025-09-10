e32fa6d0afddddd9f5f6bb470744a7d5f61b5c1c
SIDHARTH T <sidhartht900@gmail.com>
2025-09-10 23:29:45 +0530

    SignUp Page completed

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -2,7 +2,7 @@ const HTTP_STATUS = require("../../config/statusCodes.js");
 // Home page Loader
 const loadHomePage = async (req, res) => {
   try {
-    res.render("home", { user: req.user || { name: "Guest" } , cartCount : req.cartCount || 2});
+    res.render("home", { user: req?.user , cartCount : req.cartCount || 2});
   } catch (error) {
     console.error("Error loading home page:", error);
     res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send("Internal Server Error");
@@ -18,7 +18,18 @@ const pageNotFound = async (req, res) => {
   }
 };
 
+// Sign Up Page Loader
+const loadSignUpPage = async (req, res) => {
+  try {
+    res.render("signUp", { user: req?.user  , cartCount : req?.cartCount || 2});
+  } catch (error) {
+    console.error("Error loading sign-up page:", error);
+    res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send("Internal Server Error");
+  }
+}
+
 module.exports = {
   loadHomePage,
   pageNotFound,
+  loadSignUpPage
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const userController = require('../controllers/user/userController.js');
 
 // Home page route
 router.get('/pageNotFound', userController.pageNotFound);
+router.get('/signUp', userController.loadSignUpPage);
 router.get('/', userController.loadHomePage);
+
 
 module.exports = router;
