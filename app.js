40565b2b295df072593dedd65d535889a064906e
SIDHARTH T <sidhartht900@gmail.com>
2025-09-10 09:05:02 +0530

    MongoDB Setupede

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const env = require('dotenv');
+const env = require('dotenv').config();
+const connectDB = require('./config/db');
+
 const app = express();
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 }); 
+
+connectDB();
 module.exports = app;
-env.config();
